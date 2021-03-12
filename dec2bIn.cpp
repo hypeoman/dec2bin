d59b4588da8f381f530b64fdd3a3e7c7d6ab3b4c
hyperoman <43721640+hypeoman@users.noreply.github.com>
2021-03-13 01:24:48 +0300

    Add files via upload

---
+++ dec2bIn.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	std::vector<int> bin;
+
+	for (;;) {
+		if (n <= 0) {
+			break;
+		}
+		bin.push_back(n % 2);
+		n /= 2;
+		
+
+	}
+
+	int bin_size = bin.size();
+
+	for (int i = 0; i <= ( bin_size) ;i++) {
+		 std::cout << bin.back();
+		 bin.pop_back();
+		
+	}
+}
